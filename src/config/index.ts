f3ee3c39247a10b9e92f14e717937b7da8e7c1b8
kegz <kegz.peach@gmail.com>
2026-06-02 20:48:59 +0200

    testing

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,7 @@ dotenv.config();
 export const config = {
   port: process.env.PORT || 8084,
   jwtSecret: process.env.JWT_SECRET || "super_secret_key",
-  mongoUri: process.env.MONGO_URI || "mongodb+srv://kegzpeach:hlHe8HN2m8TgOB1F@tangomanagement.ngouqxc.mongodb.net/tcms_results"
+  mongoUri: process.env.MONGO_URI || "sdfmongodb+srv://kegzpeach:hlHe8HN2m8TgOB1F@tangomanagement.ngouqxc.mongodb.net/tcms_results"
 };
 
 export const connectDb = async (): Promise<void> => {
